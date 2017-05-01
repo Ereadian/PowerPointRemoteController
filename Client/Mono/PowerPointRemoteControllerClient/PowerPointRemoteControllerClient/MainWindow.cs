a3649a6b72d6777049d9e9fe28670bbbd19f926f
ereadian <ereadian@gmail.com>
2017-04-30 22:41:03 -0700

    new client UI

---
+++ Client/Mono/PowerPointRemoteControllerClient/PowerPointRemoteControllerClient/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnNextPage (object sender, EventArgs e)
+	{
+		this.TimeLabel.Text = "Next";
+	}
+
+	protected void OnPreviousPage (object sender, EventArgs e)
+	{
+		this.TimeLabel.Text = "Previous";
+	}
+}
